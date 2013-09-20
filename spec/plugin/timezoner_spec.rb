e4da9cf1496132169a2cb9f9b569f9aef0a7643e
Michael de Silva <michael@mwdesilva.com>
2013-09-20 10:53:44 +0530

    Initial spec

--- spec/plugin/timezoner_spec.rb
+++ spec/plugin/timezoner_spec.rb
@@ -5,4 +5,9 @@ describe Timezoner do
   it "should be a Module" do
     expect(Timezoner.is_a?(Module)).to be
   end
+
+  it "should ..." do
+    expect(Time.zone).to eq(ActiveSupport::TimeZone.new('UTC'))
+    binding.pry
+  end
 end
